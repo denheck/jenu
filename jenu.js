de24c0ebd09fde8389975d3ddf2d8accd6bd1ab0
Dennis Heckman <dennis.heckman@vertive.com>
2013-01-31 09:49:10 -0600

    fix bugs found while testing

--- jenu.js
+++ jenu.js
@@ -1,36 +1,51 @@
 // jenu the javascript menu enhancer
 
-(function() {
-    // utility functions
-    var getbyId = document.getElementById;
-    var each = function (items, callback) {
-        for (var i = 0; i < items.length; i++) {
-            callback.call(items[i]);
-        }
+(function(global) {
+    var methods = {
+        // utility functions
+        utility: {
+            getById: function(id) {
+                return document.getElementById(id);
+            },
+            each: function (items, callback) {
+                for (var i = 0; i < items.length; i++) {
+                    callback.call(items[i]);
+                }
 
-        return items;
-    };
-
-    var map = function (items, callback) {
-        var newItems = [];
-        for (var i = 0; i < items; i++) {
-            newItems.push(callback.call(items[i]));
-        }
-    };
-
-    var hideElement = function (element) {
-        element.style.display = 'none';
-    };
-
-    var getChildListItems = function (element) {
-        return map(element.children, function() {
-            if (this.tagName === 'LI') {
-                return this;
+                return items;
+            },
+            map: function (items, callback) {
+                var newItems = [];
+                for (var i = 0; i < items.length; i++) {
+                    newItems.push(callback.call(items[i]));
+                }
+                return newItems;
             }
-        });
+        },
+        hideElement: function (element) {
+            element.style.display = 'none';
+        },
+        getChildListItems: function (element) {
+            return map(element.children, function() {
+                if (this.tagName === 'LI') {
+                    return this;
+                }
+            });
+        },
+        getChildUnorderedLists: function (element) {
+
+        },
+        hideChildUnorderedLists: function(element) {
+
+        }
     };
 
-    var getChildUnorderedLists = function (element) {
-
+    global.jenu = {
+        init: function () {
+            // Hide all child ul elements
+        },
+        _expose: function() {
+            return methods;
+        }
     };
-})();
+})(this);
