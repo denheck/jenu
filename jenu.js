7f3242ac8cef661af9f1a2da1c04d4131eb6fdc8
Dennis Heckman <dennis.heckman@vertive.com>
2013-02-01 13:31:51 -0600

    refactor and improve menu using event delegation

--- jenu.js
+++ jenu.js
@@ -1,62 +1,133 @@
 // jenu the javascript menu enhancer
 
 (function (global) {
-    var methods = {
-        // utility functions
-        utility: {
-            getById: function (id) {
-                return document.getElementById(id);
-            },
-            each: function (items, callback) {
-                for (var i = 0; i < items.length; i++) {
-                    callback.call(items[i], items[i]);
-                }
+    // utility functions
+    var utility = {
+        each: function (items, callback) {
+            for (var i = 0; i < items.length; i++) {
+                callback.call(items[i], items[i]);
+            }
 
-                return items;
-            },
-            map: function (items, callback) {
-                var newItems = [];
-                for (var i = 0; i < items.length; i++) {
-                    newItems.push(callback.call(items[i]));
+            return items;
+        },
+        map: function (items, callback) {
+            var newItems = [];
+            for (var i = 0; i < items.length; i++) {
+                newItems.push(callback.call(items[i]));
+            }
+            return newItems;
+        },
+        filter: function (items, callback) {
+            var newItems = [];
+            this.each(items, function () {
+                if (callback.call(this) === true) {
+                    newItems.push(this);
                 }
-                return newItems;
-            },
-            filter: function (items, callback) {
-                var newItems = [];
-                this.each(items, function () {
-                    if (callback.call(this) === true) {
-                        newItems.push(this);
-                    }
-                });
-                return newItems;
+            });
+            return newItems;
+        }
+    };
+
+    var dom = {
+        getById: function (id) {
+            return document.getElementById(id);
+        },
+        attachEvent: function (element, event, callbackFunction) {
+            if (element.addEventListener) {
+                element.addEventListener(event, callbackFunction, false);
+            } else if (element.attachEvent) {
+                element.attachEvent('on' + event, callbackFunction);
             }
         },
         hideElement: function (element) {
+            if (!element) {
+                return;
+            }
+
             element.style.display = 'none';
         },
+        showElement: function (element) {
+            if (!element) {
+                return;
+            }
+
+            element.style.display = 'block';
+        },
         getChildren: function (element, tagName) {
-            return this.utility.filter(element.children, function () {
+            if (!element || !element.hasOwnProperty('children')) {
+                return [];
+            }
+
+            return utility.filter(element.children, function () {
                 if (this.tagName === tagName) {
                     return true;
                 }
             });
         },
-        hideChildUnorderedLists: function (parentElement) {
-            this.utility.each(this.getChildren(parentElement, 'UL'), this.hideElement);
+        hasChild: function (element, tagName) {
+            return this.getChildren(element, tagName).length > 0;
+        },
+        hideChildren: function (parentElement, tagName) {
+            utility.each(this.getChildren(parentElement, tagName), this.hideElement);
+        },
+        getSiblings: function (element) {
+            var prevElement = element.previousElementSibling;
+            var nextElement = element.nextElementSibling;
+            var siblings = [];
+
+            while (prevElement) {
+                siblings.push(prevElement);
+                prevElement = prevElement.previousElementSibling
+            }
+
+            while (nextElement) {
+                siblings.push(nextElement);
+                nextElement = nextElement.nextElementSibling
+            }
+
+            return siblings;
+        }
+    };
+
+    var menu = {
+        flyout: function (event) {
+            var targetElement = event.target;
+
+            if (targetElement && targetElement.tagName === 'LI') {
+                // show current target LI flyout menu
+                dom.showElement(dom.getChildren(targetElement, 'UL')[0]);
+                
+                utility.each(dom.getSiblings(targetElement), function () {
+                    utility.each(dom.getChildren(this, 'UL'), dom.hideElement);
+                });
+            }
+        },
+        attachFlyOutEvent: function (element) {
+            dom.attachEvent(element, 'mouseover', this.flyout);
+        },
+        init: function (ulElement) {
+            var menuListItems = dom.getChildren(ulElement, 'LI');
+            
+            // hide all LI elements containing UL elements
+            utility.each(menuListItems, function () {
+                dom.hideChildren(this, 'UL');
+            });
+
+            // event delegation for list items
+            menu.attachFlyOutEvent(ulElement)
         }
     };
 
     global.jenu = {
         init: function (menuId) {
-            // hide sub menus
-            var menuElement = methods.utility.getById(menuId);
-            methods.utility.each(menuId.children, methods.hideChildUnorderedLists);
-
-            // add hover/click event to LIs with ULs
-            // show flyout menu when triggered
+            menu.init(dom.getById(menuId));
         },
         _expose: function () {
-            return methods;
+            return {
+                menu: menu,
+                dom: dom,
+                utility: utility
+            };
         }
     };
 })(this);
